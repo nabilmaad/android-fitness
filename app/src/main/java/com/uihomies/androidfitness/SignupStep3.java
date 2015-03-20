0ac945fc1d3c1f589c646fa284ed4d11f6c557e8
Nabil Maadarani <nabil.maadarani@hotmail.com>
2015-03-20 18:29:53 -0400

    Added 3rd signup page (date of birth). Error validation has been thouroughly enforced. It's bullet proof.

--- app/src/main/java/com/uihomies/androidfitness/SignupStep3.java
+++ app/src/main/java/com/uihomies/androidfitness/SignupStep3.java
@@ -1,15 +1,30 @@
 package com.uihomies.androidfitness;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.uihomies.androidfitness.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class SignupStep3 extends ActionBarActivity {
+    private final static int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    private final static String dayError = "Day must be a number between 1 and 31";
+    private final static String monthError = "Month must be a number between 1 and 12";
+    private final static String yearError = "Year must be between " + (currentYear - 100) + " and " + currentYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,91 @@ public class SignupStep3 extends ActionBarActivity {
         TextView tv3=(TextView)findViewById(R.id.nextButton);
         Typeface face3=Typeface.createFromAsset(getAssets(),"fonts/DS_Marker_Felt.ttf");
         tv3.setTypeface(face3);
+
+        // Adding text watchers
+        final EditText day = (EditText)findViewById(R.id.day);
+        final EditText month = (EditText)findViewById(R.id.month);
+        final EditText year = (EditText)findViewById(R.id.year);
+
+        // Day
+        day.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                // TODO Auto-generated method stub
+                if(day.getText().toString().length()==2)
+                {
+                    // Error handling
+                    if(!day.getText().toString().matches("[0-9][0-9]") ||
+                            Integer.parseInt(day.getText().toString()) < 1 ||
+                                Integer.parseInt(day.getText().toString()) > 31)
+                        day.setError(dayError);
+                    else
+                        month.requestFocus();
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+                // TODO Auto-generated method stub
+
+            }
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        // Month
+        month.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                // TODO Auto-generated method stub
+                if(month.getText().toString().length()==2)
+                {
+                    // Error handling
+                    if(!month.getText().toString().matches("[0-9][0-9]") ||
+                            Integer.parseInt(month.getText().toString()) < 1 ||
+                                Integer.parseInt(month.getText().toString()) > 12)
+                        month.setError(monthError);
+                    else
+                        year.requestFocus();
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+                // TODO Auto-generated method stub
+
+            }
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        // Year
+        year.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                // TODO Auto-generated method stub
+                if(year.getText().toString().length()==4)
+                {
+                    // Error handling
+                    if(!year.getText().toString().matches("[0-9][0-9][0-9][0-9]") ||
+                            Integer.parseInt(year.getText().toString()) < (currentYear- 100) ||
+                                Integer.parseInt(year.getText().toString()) > currentYear)
+                        year.setError(yearError);
+                    else {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(year.getWindowToken(), 0);
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+                // TODO Auto-generated method stub
+
+            }
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
 
@@ -55,5 +155,45 @@ public class SignupStep3 extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void nextButtonClick(View view) {
+        EditText day = (EditText)findViewById(R.id.day);
+        EditText month = (EditText)findViewById(R.id.month);
+        EditText year = (EditText)findViewById(R.id.year);
+
+        String dayString = day.getText().toString();
+        String monthString = month.getText().toString();
+        String yearString = year.getText().toString();
+
+        // Error handling
+        boolean valid = true;
+        if(dayString.length() == 0 || dayString.equals("0")) {
+            day.setError(dayError);
+            valid = false;
+        }
+        if(monthString.length() == 0 || monthString.equals("0")) {
+            month.setError(monthError);
+            valid = false;
+        }
+        if(yearString.length() == 0 || yearString.equals("0")) {
+            year.setError(monthError);
+            valid = false;
+        }
+
+        if(valid) {
+            // Save user's birthday
+            Calendar cal = Calendar.getInstance();
+            cal.clear();
+            cal.set(Integer.parseInt(yearString), Integer.parseInt(monthString) - 1, Integer.parseInt(dayString));
+            SharedPreferences sharedpreferences = getSharedPreferences("appPreferences", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putLong("userBirthday", cal.getTime().getTime());
+            editor.commit();
+
+            // Load next activity
+            Intent intent = new Intent(SignupStep3.this, SignupStep4.class);
+            startActivity(intent);
+        }
     }
 }
